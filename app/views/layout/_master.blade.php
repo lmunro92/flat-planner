fd8f9626f47decdb93e0fd2bc67cfefe9894abc6
seitz.jon@gmail.com <jonathan_seitz@harvard.edu>
2014-07-25 10:07:54 -0400

    starting views

---
+++ app/views/layout/_master.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	@yield('head')
+	<meta charset="UTF-8"></meta>
+	<title>@yield('title', 'Flat-Planner')</title>
+</head>
+<body>
+	@yield('content')
+</body>
+</html
