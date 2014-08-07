bc8ec9909fd5d7d0f05662944004f6e0c2eed913
seitz.jon@gmail.com <jonathan_seitz@harvard.edu>
2014-08-06 22:30:47 -0400

    Adding Boxfile and production db information

---
+++ app/config/production/database.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+
+    'connections' => array(
+
+        'mysql' => array(
+            'driver'    => 'mysql',
+            'host'      => 'tunnel.pagodabox.com:3306',
+            'database'  => 'flat_planner',
+            'username'  => 'tomasa',
+            'password'  => 'qMEljJez',
+        ),
+
+    ),
+
+);
